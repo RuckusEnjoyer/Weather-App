2e10890fcb64f98d768c29a559fa12ccb4606a43
Kork <hail3yp@gmail.com>
2024-02-14 11:51:13 -0500

    adding cloudy/rainy/clear to weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,25 +38,54 @@ var getWeather = function(searchCity) {
     cityNameEl.text(cityName);
     
     var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=6475b3b0cca2c5c3858a7b09aadefc7c`;
-    
+
     fetch(apiUrl)
     .then(function(response){
       return response.json();
     })
     .then(function (data){
+      // // Get the weather condition
+      // let weatherCondition = data.list[0].weather[0].main.toLowerCase();
+
+      // // Determine the image URL based on the weather condition
+      // let imageUrl;
+      // switch (weatherCondition) {
+      //   case 'clear':
+      //     imageUrl = './assets/images/sunny.png';
+      //     break;
+      //   case 'clouds':
+      //     imageUrl = './assets/images/cloudy.png';
+      //     break;
+      //   case 'rain':
+      //     imageUrl = './assets/images/rainy.png';
+      //     break;
+      //   // Add more cases as needed...
+      //   default:
+      //     imageUrl = '../images/default.png';
+      // }
+
+      // // Create an image element and set its source to the image URL
+      // let weatherImage = $('<img>').attr('src', imageUrl);
+
+      // // Append the image to the page
+      // $('#weather-image').empty().append(weatherImage);
+
       //elements on page
       let humidityEl = $('#humidity');
+      let weatherEl = $('#weather');
       let tempEl = $('#temperature');
       let windEl = $('#wind');
       let fiveDaysEl = $('#5-days');
 
       fiveDaysEl.empty()
       // Storing data in variables
+      let weather = data.list[0].weather[0].main
       let humidity = data.list[0].main.humidity; // Use data.list[0].main.humidity to get the humidity
       let temp = data.list[0].main.temp; // Use data.list[0].main.temp to get the temperature
       let wind = data.list[0].wind.speed; // Use data.list[0].wind.speed to get the wind speed
 
       // Update the elements on the page
+      weatherEl.text("Weather: " + weather)
       humidityEl.text("Humidity: " + humidity);
       tempEl.text("Temperature: " + temp);
       windEl.text("Wind Speed: " + wind);
@@ -67,6 +96,7 @@ var getWeather = function(searchCity) {
             let date = new Date(forecast.dt * 1000).toLocaleDateString();
             let temp = forecast.main.temp;
             let humidity = forecast.main.humidity;
+            
             // let cardEl = $('<div>').addClass('5dayscard')
             let forecastEl = $('<div>').addClass('forecast');
             let dateEl = $('<p>').text(date).addClass('dateEl');
